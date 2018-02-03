7443fbe87f7842c7bb10afe1fd16d58dd7b75cb0
teruya <abas.trace.on@gmail.com>
2018-02-03 16:26:06 +0900

    Squashed 'DistanceVisualizer/' content from commit 13f1477
    git-subtree-dir: DistanceVisualizer
    git-subtree-split: 13f147703a84c5535fa1b7480666656f21f54fe8

---
+++ dbcontroller.py
@@ -0,0 +1,22 @@
+import MySQLdb
+
+
+def mysql_connect(host, user, passwd, db, charset='utf8'):
+    conn = MySQLdb.connect(host, user, passwd, db, charset=charset)
+    cur = conn.cursor(MySQLdb.cursors.DictCursor)
+    return conn, cur
+
+
+def select_latest(conn, cur, dev_mac, rpimac):
+    cur.execute("""SELECT *
+                   FROM distance AS m
+                   WHERE id = (
+                      SELECT MAX(id) FROM distance as s
+                      WHERE s.macaddr=%s AND (s.rpimac=%s OR s.rpimac=%s)
+                    )
+                   """, (dev_mac, rpimac[0], rpimac[1]))
+    conn.commit()
+    data = cur.fetchone()
+    print("DB:{}, pwr:{}".format(data["id"], data["pwr"]))
+    return data
+
