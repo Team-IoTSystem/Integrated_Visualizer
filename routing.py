42e2d238f1d7ab9441164bc5bd7d93b1d42f5453
teruya <abas.trace.on@gmail.com>
2018-03-03 11:27:19 +0900

    jinjaとangularにおける'{{'の衝突を回避した

--- routing.py
+++ routing.py
@@ -2,11 +2,22 @@ from flask import Flask, render_template, Markup
 from DistanceVisualizer.heatmap import main
 app = Flask(__name__)
 
+jinja_options = app.jinja_options.copy()
 
-@app.route('/heatmap')
-def show_heatmap():
+jinja_options.update(dict(
+    block_start_string='<%',
+    block_end_string='%>',
+    variable_start_string='%%',
+    variable_end_string='%%',
+    comment_start_string='<#',
+    comment_end_string='#>'
+))
+app.jinja_options = jinja_options
+
+@app.route('/')
+def show_dashboard():
     d3map = Markup(main())
-    return render_template('TEST/dashboard.html', heatmap=d3map)
+    return render_template('index.html', heatmap=d3map)
 
 if __name__ == '__main__':
     app.run()
